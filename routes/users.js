7716a682e9b85f93baef4a390a8151e043708ea1
christopherchueng <christopherchueng@gmail.com>
2022-05-09 14:07:58 -0400

    User Login Route and Login Pug File"

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,23 @@
 var express = require('express');
 var router = express.Router();
+const { csrfProtection, asyncHandler } = require('./utils.js');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
+
+router.get('/login', csrfProtection, (req, res) => {
+  res.render('user-login', {
+    title: 'Login',
+    csrfToken: req.csrfToken()
+  })
+})
+
+router.post('/login', csrfProtection, asyncHandler(async (req, res) => {
+  const { email, password } = req.body;
+
+
+}));
 
 module.exports = router;
