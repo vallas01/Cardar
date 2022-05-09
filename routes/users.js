99f948cfa6e10185088ca7d184259dc768fe80cc
christopherchueng <christopherchueng@gmail.com>
2022-05-09 15:49:54 -0400

    User Login Route

--- routes/users.js
+++ routes/users.js
@@ -36,6 +36,15 @@ const userValidators = [
       .withMessage('Please provide a value for Confirm Password')
 ];
 
+const loginValidators = [
+  check('emailAddress')
+    .exists({ checkFalsy: true })
+    .withMessage('Please provide a value for Email Address'),
+  check('password')
+    .exists({ checkFalsy: true })
+    .withMessage('Please provide a value for Password'),
+];
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
@@ -94,9 +103,33 @@ router.get('/login', csrfProtection, (req, res) => {
   })
 })
 
-router.post('/login', csrfProtection, asyncHandler(async (req, res) => {
+router.post('/login', csrfProtection, loginValidators, asyncHandler(async (req, res) => {
   const { email, password } = req.body;
 
+  let errors = [];
+  const validatorErrors = validationResult(req);
+
+  if (validatorErrors.isEmpty()) {
+    const user = await db.User.findOne({where: { email }})
+
+    if (user !== null) {
+      const passwordMatch = await bcrypt.compare(password, user.hashedPassword.toString());
+
+      if (passwordMatch) {
+        loginUser(req, res, user);
+        return res.redirect('/');
+      }
+    }
+      errors.push('Login failed for the provided email address and password.')
+  } else {
+      errors = validatorErrors.array().map((error) => error.msg);
+  }
+  res.render('user-login', {
+      title: 'Login',
+      email,
+      errors,
+      csrfToken: req.csrfToken()
+  })
 
 }));
 
