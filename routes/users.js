da8e570fbc931fc90728f32021f1ab42242b6f27
christopherchueng <christopherchueng@gmail.com>
2022-05-09 17:36:35 -0400

    typo in login validators

--- routes/users.js
+++ routes/users.js
@@ -37,7 +37,7 @@ const userValidators = [
 ];
 
 const loginValidators = [
-  check('emailAddress')
+  check('email')
     .exists({ checkFalsy: true })
     .withMessage('Please provide a value for Email Address'),
   check('password')
