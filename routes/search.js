8dfa9bda0e8b60b045ce6351431fafe1b1948791
Anthony T <anthonyt3@sbcglobal.net>
2022-05-10 12:41:45 -0700

    Redo search bar files

--- routes/search.js
+++ routes/search.js
@@ -2,11 +2,37 @@ const express = require('express');
 const db = require('../db/models');
 const router = express.Router();
 
-const { asyncHandler, searchPosts } = require('./utils');
+const { asyncHandler } = require('./utils');
 const { Post } = db;
 
 router.get('/', asyncHandler(async (req, res) => {
-    const posts = await Post.searchPosts(`%${req.query.term}%`);
+    const { searchedInput } = req.body;
+    const posts = await Post.findAll({
+      where: {
+        [Op.or]: [
+          { 
+            model: {
+              [Op.iLike]: `%${searchedInput}%`
+            }
+          },
+          {
+            make: {
+              [Op.iLike]: `%${searchedInput}%`
+            }
+          },
+          {
+            year: {
+              [Op.iLike]: `%${searchedInput}%`
+            }
+          },
+          {
+            color: {
+              [Op.iLike]: `%${searchedInput}%`
+            }
+          }
+        ]
+      }
+    })
     res.render('search', {
         title: "Search Results",
         posts
--- routes/utils.js
+++ routes/utils.js
@@ -4,40 +4,11 @@ const csrfProtection = csrf({cookie: true});
 
 const asyncHandler = (handler) => (req, res, next) => handler(req, res, next).catch(next);
 
-async function searchPosts(term) {
-    const posts = await Post.findAll({
-      where: {
-        [Op.or]: [
-          { 
-            model: {
-              [Op.iLike]: `%${term}%`
-            }
-          },
-          {
-            make: {
-              [Op.iLike]: `%${term}%`
-            }
-          },
-          {
-            year: {
-              [Op.iLike]: `%${term}%`
-            }
-          },
-          {
-            color: {
-              [Op.iLike]: `%${term}%`
-            }
-          }
-        ]
-      }
-    })
-    return posts;
-}
+
 
 
 module.exports = {
     csrfProtection,
-    asyncHandler,
-    searchPosts
+    asyncHandler
     
 }
