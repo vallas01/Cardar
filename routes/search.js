7c831b5ee0081026c2026a96e315c914faf17dc7
Anthony T <anthonyt3@sbcglobal.net>
2022-05-10 10:16:41 -0700

    Search Changes to be refactored

--- routes/search.js
+++ routes/search.js
@@ -1,8 +1,19 @@
 const express = require('express');
+const db = require('../db/models');
 const router = express.Router();
 
 const { asyncHandler, searchMakes, searchYears, searchModels } = require('./utils');
+const { Post } = db;
 
-router.get('/', )
+router.get('/', asyncHandler(async (req, res) => {
+    const models = await Post.searchModels(req.body.model);
+    const makes = await Post.searchMakes(req.body.make);
+    const years = await Post.searchYears(req.body.year);
+    res.render('search', {
+        models,
+        makes,
+        years
+    })
+}));
 
 module.exports = router;
