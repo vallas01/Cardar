bc16ff25e112425fa0d3a25df3aa84fc622b8707
christopherchueng <christopherchueng@gmail.com>
2022-05-13 15:41:14 -0400

    Seeded more data in comment, post, and image seeders.

--- db/seeders/20220510153648-postSeeds.js
+++ db/seeders/20220510153648-postSeeds.js
@@ -36,6 +36,19 @@ module.exports = {
       updatedAt: new Date()
     },
     {
+      name: 'Should I?',
+      model: '718 Boxster',
+      make: 'Porsche',
+      year: '2017',
+      color: 'orange',
+      accidents: '0',
+      features: '',
+      description: 'Might pull the trigger on this... Should I?',
+      ownerId: 1,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },
+    {
       name: 'Old but Gold!',
       model: 'Wrangler',
       make: 'Jeep',
@@ -75,6 +88,19 @@ module.exports = {
       updatedAt: new Date()
     },
     {
+      name: 'Should I Sell?',
+      model: 'Ioniq 5',
+      make: 'Hyundai',
+      year: '2022',
+      color: 'white',
+      accidents: '0',
+      features: '',
+      description: 'Interior view.',
+      ownerId: 3,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },
+    {
       name: 'New Family Member',
       model: 'X',
       make: 'Tesla',
@@ -99,7 +125,85 @@ module.exports = {
       ownerId: 4,
       createdAt: new Date(),
       updatedAt: new Date()
-    }
+    },
+    {
+      name: 'New Splashes',
+      model: 'Continental',
+      make: 'Bentley',
+      year: '2022',
+      color: 'yellow',
+      accidents: '0',
+      features: '',
+      description: 'Look what\'s back from the paint shop. Anyone want?',
+      ownerId: 2,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },
+    {
+      name: 'Rolling in Vacation',
+      model: 'Ghost',
+      make: 'Rolls Royce',
+      year: '2020',
+      color: 'white',
+      accidents: '0',
+      features: 'Champagne Cooler',
+      description: 'Traveling in luxury. Yes, it was nice. You don\'t have to tell me twice.',
+      ownerId: 1,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },
+    {
+      name: 'My First Car',
+      model: '250 GTO',
+      make: 'Ferrari',
+      year: '1962',
+      color: 'Red',
+      accidents: '0',
+      features: '',
+      description: 'One of a kind.',
+      ownerId: 2,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },
+    {
+      name: 'Selling Lexus',
+      model: 'LX 570',
+      make: 'Lexus',
+      year: '2018',
+      color: 'Black',
+      accidents: '0',
+      features: '',
+      description: 'Anyone want? It has been sitting in my garage collecting dust.',
+      ownerId: 3,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },
+    {
+      name: 'Ford Transit 4 Sale',
+      model: 'Passenger Van XL',
+      make: 'Ford',
+      year: '2020',
+      color: 'Black',
+      accidents: '0',
+      features: '',
+      description: 'Selling for a friend. DM me!',
+      ownerId: 3,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },
+    {
+      name: 'NIO Coming Soon...',
+      model: 'ES8',
+      make: 'NIO',
+      year: '2022',
+      color: 'Black',
+      accidents: '0',
+      features: '6 Seater',
+      description: 'Excited to see these on the road! Coming soon!',
+      ownerId: 4,
+      createdAt: new Date(),
+      updatedAt: new Date()
+  }
    ], {});
   },
 
--- db/seeders/20220510153649-imageSeeds.js
+++ db/seeders/20220510153649-imageSeeds.js
@@ -9,49 +9,97 @@ module.exports = {
       Example:
       */
    return queryInterface.bulkInsert('Images', [
-    {
-      path: 'https://media.ed.edmunds-media.com/for-sale/54-zhwua6zx1mla17776/img-1-600x400.jpg',
-      postId: 1,
-      createdAt: new Date(),
-      updatedAt: new Date()
-    },
-    {
-      path: 'https://cdn-ds.com/blogs-media/sites/603/2019/04/11082450/20109-Nissan-GT-R-NISMO-ext1_o.jpg',
-      postId: 2,
-      createdAt: new Date(),
-      updatedAt: new Date()
-    },
-    {
-      path: 'https://cdn1.mecum.com/auctions/pa0819/pa0819-388563/images/1-1562852447858@2x.jpg?1564083798000',
-      postId: 3,
-      createdAt: new Date(),
-      updatedAt: new Date()
-    },
-    {
-      path: 'https://robbreport.com/wp-content/uploads/2021/05/1-13.jpg?w=1000',
-      postId: 4,
-      createdAt: new Date(),
-      updatedAt: new Date()
-    },
-    {
-      path: 'https://i.ytimg.com/vi/XF6VsyqXwFg/maxresdefault.jpg',
-      postId: 5,
-      createdAt: new Date(),
-      updatedAt: new Date()
-    },
-    {
-      path: 'https://static1.hotcarsimages.com/wordpress/wp-content/uploads/2020/09/Tesla-Model-X-P100D-1.jpg',
-      postId: 6,
-      createdAt: new Date(),
-      updatedAt: new Date()
-    },
-    {
-      path: 'https://www.teslarati.com/wp-content/uploads/2020/09/Lucid-Air-5.png',
-      postId: 7,
-      createdAt: new Date(),
-      updatedAt: new Date()
-    }
-   ], {});
+      {
+        path: 'https://media.ed.edmunds-media.com/for-sale/54-zhwua6zx1mla17776/img-1-600x400.jpg',
+        postId: 1,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        path: 'https://cdn-ds.com/blogs-media/sites/603/2019/04/11082450/20109-Nissan-GT-R-NISMO-ext1_o.jpg',
+        postId: 2,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        path: 'https://cdn1.mecum.com/auctions/pa0819/pa0819-388563/images/1-1562852447858@2x.jpg?1564083798000',
+        postId: 3,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        path: 'https://robbreport.com/wp-content/uploads/2021/05/1-13.jpg?w=1000',
+        postId: 4,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        path: 'https://i.ytimg.com/vi/XF6VsyqXwFg/maxresdefault.jpg',
+        postId: 5,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        path: 'https://static1.hotcarsimages.com/wordpress/wp-content/uploads/2020/09/Tesla-Model-X-P100D-1.jpg',
+        postId: 6,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        path: 'https://www.teslarati.com/wp-content/uploads/2020/09/Lucid-Air-5.png',
+        postId: 7,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        path: 'https://cars.usnews.com/pics/size/776x517/images/Auto/custom/14652/2022_Hyundai_Ioniq_5_22.jpg',
+        postId: 8,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        path: 'https://hips.hearstapps.com/hmg-prod/images/2022-bentley-continental-gt-speed-128-1620193617.jpg?crop=0.675xw:0.493xh;0.162xw,0.257xh&resize=1200:*',
+        postId: 9,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        path: 'https://cimg1.ibsrv.net/ibimg/hgm/1920x1080-1/100/548/2017-porsche-718-boxster-s-2016-geneva-motor-show_100548253.jpg',
+        postId: 10,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        path: 'https://img.hmn.com/900x0/stories/2018/06/1962-Ferrari-250-GTO-by-Scaglietti_33.jpg',
+        postId: 11,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        path: 'https://imageio.forbes.com/specials-images/imageserve/5f4e2f719ea3d23d78b167b8/New-2020-Rolls-Royce-Ghost/960x0.jpg?fit=bounds&format=jpg&width=960',
+        postId: 12,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        path: 'https://lexus-cms-media.s3.us-east-2.amazonaws.com/wp-content/uploads/2019/04/Lexus_LXI_frontthreequarter_003_2988C56D337C7D0A30CBD34F4A119E687CB740AA.jpg',
+        postId: 13,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        path: 'https://www.autotrader.com/wp-content/uploads/2020/02/2020-Ford-Transit-.1..jpg',
+        postId: 14,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        path: 'https://www.carscoops.com/wp-content/uploads/2019/03/cad8840d-nio-es8-tuning-revozport-00.jpg',
+        postId: 15,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      }
+  ], {});
   },
 
   down: (queryInterface, Sequelize) => {
