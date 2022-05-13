ee64aa44515c8ed27c7f0aeb6911af1d0c6b43eb
christopherchueng <christopherchueng@gmail.com>
2022-05-13 11:03:48 -0400

    Refactored Error routes

--- app.js
+++ app.js
@@ -52,16 +52,21 @@ app.use('/search', searchRouter);
 //   next(createError(404));
 // });
 
-// Error handler for 404 errors.
+// Catch unhandled requests and forward to error handler.
 app.use((req, res, next) => {
-  const err = new Error;
-  err.status = 404
-  res.status(404);
-  res.render('page-not-found', {
-    title: 'Page Not Found',
-  });
+  const err = new Error('The requested page couldn\'t be found.');
+  err.status = 404;
+  next(err);
+});
 
-  if (err.status !== 404) {
+// Error handler for 404 errors.
+app.use((err, req, res, next) => {
+  if (err.status === 404) {
+    res.status(404);
+    res.render('page-not-found', {
+      title: 'Page Not Found',
+    });
+  } else {
     next(err);
   }
 });
@@ -77,6 +82,15 @@ app.use(function (err, req, res, next) {
   res.render('error');
 });
 
-
+// // Generic error handler.
+// app.use((err, req, res, next) => {
+//   res.status(err.status || 500);
+//   const isProduction = environment === 'production';
+//   res.render('error', {
+//     title: 'Server Error',
+//     message: isProduction ? null : err.message,
+//     stack: isProduction ? null : err.stack,
+//   });
+// });
 
 module.exports = app;
--- routes/users.js
+++ routes/users.js
@@ -47,9 +47,9 @@ const loginValidators = [
 ];
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+// router.get('/', function(req, res, next) {
+//   res.send('respond with a resource');
+// });
 
 router.get('/register', csrfProtection, (req, res) => {
   const user = db.User.build({});
