72318c7306fce8e5edbfb1ea50ea2ec32e724b0e
vallas01 <vallasac@gmail.com>
2022-05-13 11:54:03 -0400

    Friday morning merge

--- routes/posts.js
+++ routes/posts.js
@@ -137,6 +137,26 @@ router.get('/:id(\\d+)', asyncHandler(async(req, res) => {
     })
 }));
 
+router.put('/edit/:id(\\d+)', postValidators, requireAuth, asyncHandler(async (req, res) => {
+    console.log("**********got here*****", req.body)
+    // const postIdEdit = parseInt(req.params.id, 10);
+    const postEdit = await db.Post.findByPk(req.params.id);
+
+    const validatorErrors = validationResult(req);
+
+    if (validatorErrors.isEmpty()) {
+        await post.save();
+        return res.redirect(`/users/${userId}`);
+    } else {
+      const errors = validatorErrors.array().map((error) => error.msg);
+      res.json({
+          message: `Post Edit Failed`
+      })
+    }
+}))
+
+
+
 router.delete('/:id(\\d+)', asyncHandler(async(req, res) => {
 
     const postId = parseInt(req.params.id, 10);
